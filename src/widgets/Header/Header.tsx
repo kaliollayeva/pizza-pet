9660db0791ad7092ae6dce23193d9209030856a9
Akbota Kaliollayeva <kaliollaeva.b@gmail.com>
2025-12-14 18:56:05 +0500

    Initial commit

---
+++ src/widgets/Header/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import styles from "./Header.module.css";
+
+export default function Header (){
+    return (
+        <div className={styles.header}>
+            <h2 className='logo'>pizzashop</h2>
+            <nav className={styles.nav}>
+                <Link className={styles.link} href="/">Home</Link>
+                <Link className={styles.link} href="/menu/page">Menu</Link>
+                <Link className={styles.link} href="/events/page">Events</Link>
+                <Link className={styles.link} href="/about/page">About us</Link>
+            </nav>
+            <p>
+                Log In
+            </p>
+        </div>
+    )
+}
