357f6442dae8f9cb0b0831f414fc76d4154f1176
Akbota Kaliollayeva <kaliollaeva.b@gmail.com>
2025-12-28 00:35:33 +0500

    feat: component Button is added

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+import styles from './Button.module.css'
+
+type ButtonVariant = 'primary' | 'secondary' |'outline' ;
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+  children?: ReactNode;
+}
+
+export default function Button({
+  children, variant = 'primary',
+  disabled, className = '', ...props}: ButtonProps){
+
+  const buttonClasses = [
+    styles.button,
+    styles[variant],
+    className
+  ].filter(Boolean).join(' ');
+
+  return (
+    <button className={buttonClasses} disabled={disabled} {...props}>
+      {children}
+    </button>
+  );
+}
