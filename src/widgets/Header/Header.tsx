b33266411271885a7e2d0f6e5305a96e16614916
Akbota Kaliollayeva <kaliollaeva.b@gmail.com>
2026-02-10 23:01:33 +0500

    feat: layout and styling added

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,29 +1,55 @@
+'use client';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from './Header.module.css';
 import Button from '../../shared/ui/Button/Button';
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <h2 className="logo">pizzashop</h2>
-      <nav className={styles.nav}>
-        <Link className={styles.link} href="/">
+      <nav className={`${styles.nav} ${isOpen ? styles.navOpen : ''}`}>
+        <Link href="/" className={`${styles.link} ${pathname === '/' ? styles.active : ''}`}>
           Home
         </Link>
-        <Link className={styles.link} href="/menu/page">
+        <Link
+          href="/menu/page"
+          className={`${styles.link} ${pathname === '/menu/page' ? styles.active : ''}`}
+        >
           Menu
         </Link>
-        <Link className={styles.link} href="/events/page">
+        <Link
+          href="/events/page"
+          className={`${styles.link} ${pathname === '/events/page' ? styles.active : ''}`}
+        >
           Events
         </Link>
-        <Link className={styles.link} href="/about/page">
+        <Link
+          href="/about/page"
+          className={`${styles.link} ${pathname === '/about/page' ? styles.active : ''}`}
+        >
           About us
         </Link>
       </nav>
 
-      <div className={styles.button}>
-        <Button variant={'primary'}>Log in</Button>
+      <div className={styles.buttonsContsiner}>
+        <div className={styles.buttonLogin}>
+          <Button variant={'primary'}>Log in</Button>
+        </div>
+        <div className={styles.buttonCircle}>
+          <Button variant={'primary'}>
+            <img src="/basket.svg" alt="Корзина" width="20" height="20" />
+          </Button>
+        </div>
+        <div className={styles.burgerButton}>
+          <Button onClick={() => setIsOpen(!isOpen)}>
+            <img src="/burgermenu.png" alt="Корзина" width="40" height="20" />
+          </Button>
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
