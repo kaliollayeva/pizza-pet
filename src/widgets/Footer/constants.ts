4415bc819f5682cec29ccfd20a0fc962686bfa81
Akbota Kaliollayeva <kaliollaeva.b@gmail.com>
2026-03-01 23:47:34 +0500

    feat: separated svg, constant links

---
+++ src/widgets/Footer/constants.ts
@@ -0,0 +1,35 @@
+export const navConfig = [
+  {
+    title: 'Home',
+    links: [
+      { subTitle: 'To Order', href: '/' },
+      { subTitle: 'About us', href: '/about' },
+      { subTitle: 'Events', href: '/events' },
+      { subTitle: 'Menu', href: '/menu' },
+    ],
+  },
+  {
+    title: 'Events',
+    links: [
+      { subTitle: '3 Pizza 1 Free Coffee', href: '/' },
+      { subTitle: '2 Pizza for 1 Price', href: '/' },
+      { subTitle: 'Kitchen Tour', href: '/' },
+    ],
+  },
+  {
+    title: 'Menu',
+    links: [
+      { subTitle: 'Show All', href: '/' },
+      { subTitle: 'Seaproducts', href: '/' },
+      { subTitle: 'Vegan', href: '/' },
+      { subTitle: 'Meats', href: '/' },
+    ],
+  },
+  {
+    title: 'About Us',
+    links: [
+      { subTitle: 'Our History', href: '/' },
+      { subTitle: 'Why we?', href: '/' },
+    ],
+  },
+];
