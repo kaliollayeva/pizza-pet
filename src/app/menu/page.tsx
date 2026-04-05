944b2631f72165bbe15cbf0ed0ec6a71c3130b4c
Akbota Kaliollayeva <kaliollaeva.b@gmail.com>
2026-04-05 20:49:27 +0500

    feat: компонент страницы Menu

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,1 +1,74 @@
+'use client';
 import styles from './page.module.css';
+import Card from '@/widgets/Card/Card';
+import Button from '@/shared/ui/Button/Button';
+
+export default function Menu() {
+  return (
+    <div className={styles.menu}>
+
+      <h2 className="title">Menu</h2>
+
+      <ul className={styles.categoryList}>
+        <li>
+          <Button variant={'primary'} className={styles.categoryButton}>
+            Show All
+          </Button>
+        </li>
+        <li>
+          <Button variant={'secondary'} className={styles.categoryButton}>
+            Meat
+          </Button>
+        </li>
+        <li>
+          <Button variant={'secondary'} className={styles.categoryButton}>
+            Vegetarian
+          </Button>
+        </li>
+        <li>
+          <Button variant={'secondary'} className={styles.categoryButton}>
+            Sea products
+          </Button>
+        </li>
+        <li>
+          <Button variant={'secondary'} className={styles.categoryButton}>
+            Mushroom
+          </Button>
+        </li>
+      </ul>
+
+        <ul className={styles.cardList}>
+          <li>
+            <Card />
+          </li>
+          <li>
+            <Card />
+          </li>
+          <li>
+            <Card />
+          </li>
+          <li>
+            <Card />
+          </li>
+        </ul>
+
+      <div className={styles.banner}>
+        <p className={styles.bannerText}>MOST POPULAR PIZZA</p>
+      </div>
+        <ul className={styles.cardList}>
+          <li>
+            <Card />
+          </li>
+          <li>
+            <Card />
+          </li>
+          <li>
+            <Card />
+          </li>
+          <li>
+            <Card />
+          </li>
+        </ul>
+    </div>
+  );
+}
